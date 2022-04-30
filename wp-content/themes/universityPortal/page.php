904af875a31ee9f4ea9aa04b014ca05d3a4c54a3
Brian <briantechdom@gmail.com>
2022-04-30 11:04:25 -0700

    Add single and page files

--- wp-content/themes/universityPortal/page.php
+++ wp-content/themes/universityPortal/page.php
@@ -1,30 +1,14 @@
-<?php 
-get_header();
-
-?>
-
-
 <?php
       // wordpress function, tells WP while there are posts, post them.
       while(have_posts()) {
  
         // tells WP to get all relevant info of the next posts to use
         the_post(); ?>
+	  <h1> This is a page, not a post </h1>
             <!-- title for the post -->
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><?php the_title(); ?></h2>
         <!-- content for the post -->
         <?php the_content(); ?>
-        <hr>
       <?php }
  
-?>
-
-
-
-
-
-
-<?php 
-get_footer();
-
 ?>
--- wp-content/themes/universityPortal/single.php
+++ wp-content/themes/universityPortal/single.php
@@ -1,9 +1,3 @@
-<?php 
-get_header();
-
-?>
-
-
 <?php
       // wordpress function, tells WP while there are posts, post them.
       while(have_posts()) {
@@ -11,20 +5,9 @@ get_header();
         // tells WP to get all relevant info of the next posts to use
         the_post(); ?>
             <!-- title for the post -->
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><?php the_title(); ?></h2>
         <!-- content for the post -->
         <?php the_content(); ?>
-        <hr>
       <?php }
  
-?>
-
-
-
-
-
-
-<?php 
-get_footer();
-
 ?>
