bb59e6fea00abc7045042ee38cc1a83b06271976
Brian <briantechdom@gmail.com>
2022-05-03 15:02:50 -0700

    Importing css from /build folder

--- wp-content/themes/universityPortal/functions.php
+++ wp-content/themes/universityPortal/functions.php
@@ -1,12 +1,10 @@
 <?php
-
 // create function
 function university_files() {
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    // import styles from folder
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
-
 // call function
 add_action('wp_enqueue_scripts', 'university_files');
-
-
 ?>
