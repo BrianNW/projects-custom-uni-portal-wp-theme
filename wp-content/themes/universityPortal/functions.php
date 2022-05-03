ffe997d5f02dce93f1f3fdc3dff77c11fc2b8c18
Brian <briantechdom@gmail.com>
2022-05-03 15:11:00 -0700

    Imported FontAwesome CDN link

--- wp-content/themes/universityPortal/functions.php
+++ wp-content/themes/universityPortal/functions.php
@@ -2,8 +2,10 @@
 // create function
 function university_files() {
     // import styles from folder
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    
 }
 // call function
 add_action('wp_enqueue_scripts', 'university_files');
