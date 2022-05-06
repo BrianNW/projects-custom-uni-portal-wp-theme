5892e14f3c1e615c78424189fc546ee9ec1225ab
Brian <briantechdom@gmail.com>
2022-05-06 13:53:07 -0700

    Update breadcrumb page indicator with the_title()

--- wp-content/themes/universityPortal/page.php
+++ wp-content/themes/universityPortal/page.php
@@ -19,7 +19,9 @@ get_header();
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
           <!-- breadcrumb box -->
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="#">
+          <i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> 
+          <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
 
