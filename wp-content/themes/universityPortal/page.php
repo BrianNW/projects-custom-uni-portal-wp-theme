8437e6cfa222b47b3d58ec265fd6e98c7647dfed
Brian <briantechdom@gmail.com>
2022-05-06 14:14:15 -0700

    Replace metabox href with get_permalink

--- wp-content/themes/universityPortal/page.php
+++ wp-content/themes/universityPortal/page.php
@@ -25,7 +25,7 @@ get_header();
         <div class="metabox metabox--position-up metabox--with-home-link">        
         <p>
           <!-- breadcrumb box -->
-          <a class="metabox__blog-home-link" href="<?php echo ""  ?>">
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink(x);  ?>">
           <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(x); ?></a> 
           <span class="metabox__main"><?php the_title(); ?></span>
         </p>
