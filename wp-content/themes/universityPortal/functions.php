a09d7829bd47c7242920678ad08615458044e6fe
Brian <briantechdom@gmail.com>
2022-05-12 17:02:00 -0700

    Register 2nd footer menu

--- wp-content/themes/universityPortal/functions.php
+++ wp-content/themes/universityPortal/functions.php
@@ -15,7 +15,8 @@ add_action('wp_enqueue_scripts', 'university_files');
 // Setup dynamic title call and register nav menus
 function uni_features(){
     register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocationOne', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
 
