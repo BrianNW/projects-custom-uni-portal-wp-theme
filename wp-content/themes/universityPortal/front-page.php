2971b906c3992e163134d2d7dc429b13980ceee1
Brian <briantechdom@gmail.com>
2022-05-17 13:56:58 -0700

    Set up blog banner

--- wp-content/themes/universityPortal/front-page.php
+++ wp-content/themes/universityPortal/front-page.php
@@ -2,7 +2,7 @@
 get_header();
 
 ?>
-
+<!-- Home page -->
 
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')?>)"></div>
