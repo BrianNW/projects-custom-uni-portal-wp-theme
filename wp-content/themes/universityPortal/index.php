c292ec5a39195ce58edee681f33ea4df5be6c0a8
Brian <briantechdom@gmail.com>
2022-06-14 16:55:08 -0700

    Add metabox and excerpt content

--- wp-content/themes/universityPortal/index.php
+++ wp-content/themes/universityPortal/index.php
@@ -15,7 +15,18 @@ get_header(); ?>
   <div class="container container--narrow page-section">
     <?php while(have_posts()) {
       the_post(); ?>
-      <div class="post-item"> <h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2></div>
+      <div class="post-item"> <h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
+
+      <div class="metabox">
+        <p>Posted by Brian on 6.14.22 in News</p>
+      </div>
+
+      <div class="generic-content">
+        <?php the_excerpt(); ?>
+        <p><a href="<?php the_permalink(); ?>">Continue Reading</a></p>
+      </div>
+    
+    </div>
     <?php }
     ?>
   </div>
