ebfb0cf7070ba56a123c8d2083b3db381bd67315
Brian <briantechdom@gmail.com>
2022-06-18 14:28:07 -0700

    Added metabox html from page.php

--- wp-content/themes/universityPortal/single.php
+++ wp-content/themes/universityPortal/single.php
@@ -16,7 +16,14 @@ get_header();
             </div>
 
             <div class="container container--narrow page-section">
-              Metabox will go here
+                  <div class="metabox metabox--position-up metabox--with-home-link">        
+              <p>
+                <!-- breadcrumb box -->
+                <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentPID);  ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> 
+                <span class="metabox__main"><?php the_title(); ?></span>
+              </p>
+            </div>
 
               <div class="generic-content"><?php the_content(); ?></div>
           </div>
