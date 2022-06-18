dc5fd421b89cc614ca43defe5fc0052b5ff846fb
Brian <briantechdom@gmail.com>
2022-06-18 14:16:57 -0700

    Added banner elements from page.php to create header banner

--- wp-content/themes/universityPortal/single.php
+++ wp-content/themes/universityPortal/single.php
@@ -5,10 +5,16 @@ get_header();
  
         // tells WP to get all relevant info of the next posts to use
         the_post(); ?>
-            <!-- title for the post -->
-        <h2><?php the_title(); ?></h2>
-        <!-- content for the post -->
-        <?php the_content(); ?>
+           <div class="page-banner">
+              <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+              <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title() ?></h1>
+                <div class="page-banner__intro">
+                  <p>Don't forget to replace me later!!</p>
+                </div>
+              </div>
+            </div>
+
       <?php }
  
  get_footer();
