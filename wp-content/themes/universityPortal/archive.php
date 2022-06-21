1419ef6d20a982177f50b6aa46a0e2ebf878457a
Brian <briantechdom@gmail.com>
2022-06-21 09:40:27 -0700

    Finalized archive section

--- wp-content/themes/universityPortal/archive.php
+++ wp-content/themes/universityPortal/archive.php
@@ -5,9 +5,12 @@ get_header(); ?>
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"> <?php if(is_category()) { echo "category name will go here";} if(is_author()) { echo " author name will go here";} ?></h1>
+        <h1 class="page-banner__title"> 
+            <!-- <?php if(is_category()) { single_cat_title();} if(is_author()) { echo 'Posts by '; the_author();} ?> -->
+            <?php the_archive_title(); ?>
+        </h1>
         <div class="page-banner__intro">
-          <p>Keep up with the latest news.</p>
+          <p><?php the_archive_description(); ?></p>
         </div>
       </div>
 </div>
