cd2af0867d7d8f4c08ceba06a5507991dae60474
Brian <briantechdom@gmail.com>
2022-06-22 11:30:56 -0700

    Move hardcoded loop into new post loop

--- wp-content/themes/universityPortal/front-page.php
+++ wp-content/themes/universityPortal/front-page.php
@@ -55,11 +55,7 @@ get_header();
           // loop references custom query above
             while($homepagePosts->have_posts()) {
               $homepagePosts->the_post(); ?>
-            <li> <?php the_title(); ?></li>
-           <?php }
-          ?>
-
-          <div class="event-summary">
+            <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
               <span class="event-summary__month">Jan</span>
               <span class="event-summary__day">20</span>
@@ -69,16 +65,10 @@ get_header();
               <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
             </div>
           </div>
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
+           <?php }
+          ?>
+
+          
 
           <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
