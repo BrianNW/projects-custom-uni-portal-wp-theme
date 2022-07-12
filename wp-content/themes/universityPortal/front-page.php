95f6bf141c2d8095487f8bc350a35d65c4a2bf14
Brian <briantechdom@gmail.com>
2022-07-12 11:47:23 -0700

    Update the h5 title to the_title();

--- wp-content/themes/universityPortal/front-page.php
+++ wp-content/themes/universityPortal/front-page.php
@@ -35,7 +35,7 @@ get_header();
               <span class="event-summary__day">25</span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
               <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
             </div>
           </div>
