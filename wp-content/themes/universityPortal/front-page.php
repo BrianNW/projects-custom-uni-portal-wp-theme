dbb97d9741c99295b12bbb32c98f1983f6f0084f
Brian <briantechdom@gmail.com>
2022-07-12 11:49:17 -0700

    Update the p tag field where the static description was and add instead wp_trim_words() function

--- wp-content/themes/universityPortal/front-page.php
+++ wp-content/themes/universityPortal/front-page.php
@@ -36,7 +36,7 @@ get_header();
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
-              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+              <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="#" class="nu gray">Learn more</a></p>
             </div>
           </div>
               <?php }           
